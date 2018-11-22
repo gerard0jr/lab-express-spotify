73b547fc2f34dd8c7622f35762868d01959af12a
Gerardo <gera@MacBook-de-Gerardo.local>
2018-11-22 12:44:18 -0600

    Consumo de API

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,3 @@
-var SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const app = express();
 
@@ -11,24 +10,6 @@ app.set('view engine', 'hbs')
 
 const spotifyRoutes = require('./routes/routes')
 app.use('/',spotifyRoutes)
-
-// Remember to paste your credentials here
-var clientId = '7d0496df2b1c42bab6fff39bf341c2d1',
-    clientSecret = '3a98028a65de43de9365fb5d79223fc8';
-
-var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
-});
-
-// Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
-});
-
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
--- starter_code/express-spotify-app/routes/routes.js
+++ starter_code/express-spotify-app/routes/routes.js
@@ -1,13 +1,35 @@
+var SpotifyWebApi = require('spotify-web-api-node')
 const express = require('express')
 const router = express.Router()
 
+var clientId = '7d0496df2b1c42bab6fff39bf341c2d1',
+    clientSecret = '3a98028a65de43de9365fb5d79223fc8';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
 router.get('/', (req,res) => {
     const {search} = req.query
-    busqueda = {
-        name: search
-    }
     if(search){
-        res.render('artists',busqueda)
+        spotifyApi.searchArtists(search, {limit: 2})
+        .then( data => {
+            console.log(data)
+           // -res.render('artists',data)
+        })
+        .catch( error => {
+            console.log(error)
+        })
+        
     } else{
         res.render('home')
     }
